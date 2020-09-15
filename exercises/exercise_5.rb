85423f6649bc535a15ecd19454d8f1e7d057ceaf
JCON3DEV <jconheasman@gmail.com>
2020-09-15 04:52:51 +0000

    ex 5.1 anmd 5.3 complete and working 5.2 still has error

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -7,4 +7,25 @@ require_relative './exercise_4'
 puts "Exercise 5"
 puts "----------"
 
-# Your code goes here ...
+@total_stores_annual_revenue = Store.sum(:annual_revenue)
+puts @total_stores_annual_revenue
+#  prints; 4114000
+puts "----------"
+
+@total_stores = Store.count()
+puts @total_stores
+# prints 5
+
+# below does NOT WORK Prints; 0  Should print 822,800
+@average_annual_revenue = (@total_store_annual_revenue.to_i / @total_stores)
+puts @average_annual_revenue
+
+puts "----------"
+@number_of_high_earning_stores = Store.where("annual_revenue < 1000000").count()
+puts @number_of_high_earning_stores
+# above prints; 3 = an integer not related to the data
+
+@high_earning_stores = Store.where("annual_revenue < 1000000")
+@high_earning_stores.each do |profitable_store|
+  puts profitable_store.name
+end
