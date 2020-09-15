94613f017910bf516c1d725b0e25d223c4c70e2b
JCON3DEV <jconheasman@gmail.com>
2020-09-15 02:35:58 +0000

    3 stores created using active record. Tables contain revenue and male / female apparel.

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -4,3 +4,35 @@ puts "Exercise 1"
 puts "----------"
 
 # Your code goes below here ...
+class Store < ActiveRecord::Base
+  # attr_accessor :name, :annual_revenue, :mens_apparel, :womens_apparel
+end
+
+burnaby = Store.new(
+  name: "burnaby",
+  annual_revenue: 300000,
+  mens_apparel: true, 
+  womens_apparel: true, 
+)
+puts burnaby
+#  save() saves it to the database. Usually used for validation before you update the database
+burnaby.save()
+
+
+richmond = Store.new(
+  name: "Richmond",
+  annual_revenue: 1260000,
+  mens_apparel: false, 
+  womens_apparel: true, 
+)
+puts richmond
+richmond.save()
+
+gastown = Store.new(
+  name: "Gastown",
+  annual_revenue: 190000,
+  mens_apparel: true, 
+  womens_apparel: false, 
+)
+puts gastown
+gastown.save()
